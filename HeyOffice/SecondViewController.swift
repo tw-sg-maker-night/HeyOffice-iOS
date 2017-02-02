72d0ec404f9c1e85e515b242dd17a815cc29a004
Colin Harris <col.w.harris@gmail.com>
2017-02-02 23:13:31 +0800

    Initial Commit

---
+++ HeyOffice/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  HeyOffice
+//
+//  Created by Colin Harris on 2/2/17.
+//  Copyright © 2017 ThoughtWorks. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
