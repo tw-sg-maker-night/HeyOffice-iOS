9e404e2cab7898f12665240936b2d67bdf0d249d
Colin Harris <col.w.harris@gmail.com>
2017-02-08 09:27:27 +0800

    Added aws-sdk

--- HeyOffice/AppDelegate.swift
+++ HeyOffice/AppDelegate.swift
@@ -7,18 +7,30 @@
 //
 
 import UIKit
+import AWSCore
+import AWSCognitoIdentityProvider
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        //setup logging
+        AWSLogger.default().logLevel = AWSLogLevel.verbose
+
+        //setup service config
+        let serviceConfig = AWSServiceConfiguration(region: CognitoIdentityUserPoolRegion, credentialsProvider: nil)
+        
+        //create a pool
+        let userPoolConfig = AWSCognitoIdentityUserPoolConfiguration(clientId: CognitoIdentityUserPoolAppClientId, clientSecret: CognitoIdentityUserPoolAppClientSecret, poolId: CognitoIdentityUserPoolId)
+        
+        AWSCognitoIdentityUserPool.register(with: serviceConfig, userPoolConfiguration: userPoolConfig, forKey: "UserPool")
+        
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
---
+++ HeyOffice/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  HeyOffice
+//
+//  Created by Colin Harris on 2/2/17.
+//  Copyright © 2017 ThoughtWorks. All rights reserved.
+//
+
+import Foundation
+import AWSCore
+
+let CognitoIdentityUserPoolRegion = AWSRegionType.USEast1
+let CognitoIdentityUserPoolAppClientId = ""
+let CognitoIdentityUserPoolAppClientSecret = ""
+let CognitoIdentityUserPoolId = ""
